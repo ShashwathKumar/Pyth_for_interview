f546a8e4e0381a56706c6c4cd3dbd6f27b764823
Shashwath Kattepura Jayabheema Rao (shkattep) <shkattep@cisco.com>
2016-08-08 20:45:01 -0700

    putEntry working hashTable cisco

--- companies/Cisco/hashTable.py
+++ companies/Cisco/hashTable.py
@@ -1,15 +1,9 @@
 HTSize = 16
 
 class HashEntry(object):
-	def __init__(self, mac=None, port=None, hashIndex=None):
-		self.entry = (mac, port, hashIndex)
+	def __init__(self, mac=None, port=None, nextHashIndex=None):
+		self.entry = [mac, port, nextHashIndex]
 		self.nextEntry = None
-
-	def getEntry(self):
-		return self.entry
-
-	def clearEntry(self):
-		self.entry = (None, None, None)
 
 class HashTable(object):
 	def __init__(self):
@@ -23,7 +17,7 @@ class HashTable(object):
 		for i in range(1,HTSize): #notice HTSize is as defined above
 			ptr2 = HashEntry()
 			ptr.nextEntry = ptr2
-			self.table.append(ptr)
+			self.table.append(ptr2)
 			ptr = ptr2
 
 	def printHashTable(self):
@@ -32,27 +26,28 @@ class HashTable(object):
 			print e.entry
 
 	def getEntry(self, mac):
-		hashIndex = calcHash(mac)
+		hashIndex = self.calcHash(mac)
 		return self.table[hashIndex]
 
 	def putEntry(self, mac, port):
-		hashIndex = calcHash(mac)
-		if not self.table[hashIndex].mac:
+		hashIndex = self.calcHash(mac)
+		if not self.table[hashIndex].entry[0]:  #this refers to mac address
 			self.table[hashIndex] = HashEntry(mac, port)
 		else:
 			#handle collision
 			emptySlot = self.uTop
 			self.uTop = self.uTop.nextEntry
-			emptySlot = HashEntry(mac,port)
+			emptySlot.entry[0]=mac
+			emptySlot.entry[1]=port
 
 			ptr = self.table[hashIndex]
-			while ptr.hashIndex:
-				newIndex = ptr.hashIndex
+			while ptr.entry[2]: #this refers to nextHashIndex
+				newIndex = ptr.entry[2]
 				ptr = self.table[newIndex]
-			ptr.hashIndex = self.table.index(emptySlot) #index of empty slot
+			ptr.entry[2] = self.table.index(emptySlot) #index of empty slot
 
-			#update uTop
-			#find next emptySlot in the list after emptySlot
+			#update sTop
+			
 
 	def calcHash(self, mac):
 		macList = mac.split(':')
@@ -60,6 +55,23 @@ class HashTable(object):
 		return hashIndex
 
 def main():
+	ht = HashTable()
+	ht.printHashTable()
+	print "------------------------------------------------"
+	ht.putEntry('12:34:4C:31:1A:1B',6)
+	ht.printHashTable()
+	print "------------------------------------------------"
+	ht.putEntry('12:34:4C:31:1A:2B',7)
+	ht.putEntry('12:34:4C:31:1A:3C',8)
+	ht.printHashTable()
+	print "------------------------------------------------"
+	ht.putEntry('12:32:12:3C:2A:3C',9)
+	ht.printHashTable()
+	print "------------------------------------------------"
+	ht.putEntry('13:3C:1B:3A:21:2B',9)
+	ht.printHashTable()
+	print "------------------------------------------------"
+
 
 if __name__ == "__main__":
 	main()
