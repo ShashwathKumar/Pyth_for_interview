336dba9176cccf59fd19b9ed0212d63e31ffc662
Shashwath Kattepura Jayabheema Rao (shkattep) <shkattep@cisco.com>
2016-08-08 22:53:27 -0700

    empty Slots working but in usual traversal way -- All Done <3

--- companies/Cisco/hashTable.py
+++ companies/Cisco/hashTable.py
@@ -25,6 +25,7 @@ class HashTable(object):
 		for e in self.table:
 			print e.entry
 		self.check_sTop()
+		self.check_uTop()
 
 	def getEntry(self, mac):
 		hashIndex = self.calcHash(mac)
@@ -35,9 +36,19 @@ class HashTable(object):
 
 		newSlot = None
 		if not self.table[hashIndex].entry[0]:  #this refers to mac address
+		    #initializing newSlot variable for updation of sTop
+			#update uTop - remove newSlot from the list of emptySlots
+			if self.table[hashIndex]==self.uTop:
+				self.uTop = self.uTop.nextEntry
+			else:
+				ptr = self.uTop
+				while ptr.nextEntry != self.table[hashIndex]:
+					ptr = ptr.nextEntry
+				ptr.nextEntry = ptr.nextEntry.nextEntry
 			self.table[hashIndex] = newSlot = HashEntry(mac, port)
 		else:
 			#handle collision
+			#update uTop
 			newSlot = self.uTop
 			self.uTop = self.uTop.nextEntry
 			newSlot.entry[0]=mac
@@ -49,14 +60,14 @@ class HashTable(object):
 				ptr = self.table[newIndex]
 			ptr.entry[2] = self.table.index(newSlot) #index of empty slot
 
-			#update sTop
+		#update sTop
 		if not self.sTop:
 			self.sTop = newSlot
 			self.sTop.nextEntry = None #This separates this entry from uTop
 		else:
 			ptr = self.sTop
 			while ptr.nextEntry:
-				print ptr.nextEntry.entry
+				#print ptr.nextEntry.entry
 				ptr = ptr.nextEntry
 			ptr.nextEntry = newSlot
 			ptr.nextEntry.nextEntry = None
@@ -64,21 +75,28 @@ class HashTable(object):
 	def check_sTop(self):
 		ptr = self.sTop
 		print "************************************"
+		print "filledSlots:"
 		while ptr:
+			#printing index here is inefficient but done just for visualization purposes
 			print ptr.entry, self.table.index(ptr)
 			ptr = ptr.nextEntry
 		print "************************************"
 
 	def check_uTop(self):
 		ptr = self.uTop
+		print "emptySlots:"
 		while ptr:
-			print ptr.entry, self.table.index(ptr)
+			#printing index here is inefficient but done just for visualization purposes
+			print ptr.entry, self.table.index(ptr) 
 			ptr = ptr.nextEntry
 
 	def calcHash(self, mac):
 		macList = mac.split(':')
 		hashIndex = int(macList[-1],16)%HTSize
 		return hashIndex
+
+	def clear(self):
+		self.__init__()
 
 def main():
 	ht = HashTable()
@@ -97,7 +115,9 @@ def main():
 	ht.putEntry('13:3C:1B:3A:21:2B',9)
 	ht.printHashTable()
 	print "------------------------------------------------"
-
+	ht.clear()
+	ht.printHashTable()
+	print "------------------------------------------------"
 
 if __name__ == "__main__":
 	main()
